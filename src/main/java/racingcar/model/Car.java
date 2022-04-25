c1a0b799931554444efd981029c710eb69186425
10M <tenmillionwon@gmail.com>
2022-04-25 09:45:16 +0900

    refactor: 자동차 매직넘버 상수화

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -4,10 +4,13 @@ import racingcar.domain.NumberStrategy;
 
 public class Car {
     private final static String INVALID_CAR_NAME = "자동차 이름은 5글자를 초과할 수 없습니다.";
+    private final static int MAX_CAR_NAME_SIZE = 5;
+    private final static int NUMBER_FOR_MOVE = 4;
+
     private final String name;
     private int position;
     public Car(String name) {
-        if (name.length() > 5) {
+        if (name.length() > MAX_CAR_NAME_SIZE) {
             throw new IllegalArgumentException(INVALID_CAR_NAME);
         }
         this.name = name;
@@ -27,7 +30,7 @@ public class Car {
     }
 
     public void moveOrStop(NumberStrategy numberStrategy) {
-        if (numberStrategy.generateNumber() >= 4) {
+        if (numberStrategy.generateNumber() >= NUMBER_FOR_MOVE) {
             moveForward();
         }
     }
